61f7f8c376fbd4fd2ebd0fa5e2da87d1ad9dfdd0
Jamal El Hizazi <elhizazi1@gmail.com>
2021-03-12 22:21:11 +0100

    Init

--- assets/RPInfo.bat
+++ assets/RPInfo.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 bash -c "cd var/mobile/NeutronFile.github.io; ./RPInfo.sh"
-=======
-bash -c "cd D/NeutronFile.github.io; ./RPInfo.sh"
->>>>>>> deb3aaa8346971406e6a035cdde6594b4128aaa6
