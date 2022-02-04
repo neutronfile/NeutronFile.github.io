f29fed7303fcf5fa47fa2af07efc041720345604
elhizazi1 <elhizazi1@gmail.com>
2022-02-04 15:07:13 +0100

    Init

--- RP.sh
+++ RP.sh
@@ -2,5 +2,5 @@
 apt-ftparchive packages ./debfiles/ > ./Packages;
 #sed -i -e '/^SHA/d' ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
-printf "Origin: NeutronFile's Repo\nLabel: NeutronFile\nSuite: stable\nVersion: 1.0\nCodename: NuetronFile\nArchitecture: iphoneos-arm\nComponents: main\nDescription: NeutronFile's Best Repo for all Packages! Cydia + Sileo + Installer + Zebra! & Saily.\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: NeutronFile's Repo\nLabel: NeutronFile\nSuite: stable\nVersion: 1.0\nCodename: NuetronFile\nArchitecture: iphoneos-arm\nHomepage: https://neutronfile.org\nTwitter: @NeutronFile <https://twitter.com/NeutronFile/>\nDonate: https://paypal.me/Elhizazi\nEmail: elhizazi1@icloud.com\nSponsor: NeutronFile.org <http://neutronfile.org/>\nComponents: main\nDescription: NeutronFile's Best Repo for all Packages! Cydia + Sileo + Installer + Zebra! & Saily.\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 exit 0;
